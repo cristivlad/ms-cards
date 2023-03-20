15dc16f7b9337e96755a6ce9dd6ea456c2669d37
Cristiii <cristian.vlad10@gmail.com>
2023-03-20 11:44:05 +0200

    updated project to use configServer and read props from it

--- src/main/java/com/example/mscards/controller/CardsController.java
+++ src/main/java/com/example/mscards/controller/CardsController.java
@@ -1,8 +1,14 @@
 package com.example.mscards.controller;
 
+import com.example.mscards.config.CardsServiceConfig;
 import com.example.mscards.model.Cards;
 import com.example.mscards.model.Customer;
+import com.example.mscards.model.Properties;
 import com.example.mscards.repository.CardsRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,13 +19,23 @@ import java.util.List;
 public class CardsController {
 
     private final CardsRepository cardsRepository;
+    private final CardsServiceConfig cardsConfig;
 
-    public CardsController(CardsRepository cardsRepository) {
+    public CardsController(CardsRepository cardsRepository, CardsServiceConfig cardsConfig) {
         this.cardsRepository = cardsRepository;
+        this.cardsConfig = cardsConfig;
     }
 
     @PostMapping("/myCards")
     public List<Cards> getCardDetails(@RequestBody Customer customer) {
         return cardsRepository.findByCustomerId(customer.getCustomerId());
+    }
+
+    @GetMapping("/cards/properties")
+    public String getPropertyDetails() throws JsonProcessingException {
+        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        Properties properties = new Properties(cardsConfig.getMsg(), cardsConfig.getBuildVersion(),
+                cardsConfig.getMailDetails(), cardsConfig.getActiveBranches());
+        return ow.writeValueAsString(properties);
     }
 }
