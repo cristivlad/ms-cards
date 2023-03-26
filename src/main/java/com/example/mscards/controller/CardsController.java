ba7c337a4fa54fca606f3a483a26355d90a946ea
cristiii <cristian.vlad10@gmail.com>
2023-03-26 23:00:50 +0300

    implemented TraceId for request and added it to header of response

--- src/main/java/com/example/mscards/controller/CardsController.java
+++ src/main/java/com/example/mscards/controller/CardsController.java
@@ -8,10 +8,7 @@ import com.example.mscards.repository.CardsRepository;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -27,7 +24,7 @@ public class CardsController {
     }
 
     @PostMapping("/myCards")
-    public List<Cards> getCardDetails(@RequestBody Customer customer) {
+    public List<Cards> getCardDetails(@RequestHeader("tmx-correlation-id") String correlationId, @RequestBody Customer customer) {
         return cardsRepository.findByCustomerId(customer.getCustomerId());
     }
 
